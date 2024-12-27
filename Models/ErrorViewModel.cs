e528c9610ecf7e4fcedc9a702a784931e5a34223
leonor-p <leonor.teixeira.pintas@gmail.com>
2024-12-27 14:49:03 +0000

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Biblioteca_UniLib.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
