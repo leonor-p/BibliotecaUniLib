5cec1ccb20f87b7b1ea804579ddb06b44928746c
Carolpm28 <pinheirocarolina44@gmail.com>
2024-12-30 23:16:33 +0000

    pagina ajuda

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Biblioteca_UniLib.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,10 +8,12 @@ namespace Biblioteca_UniLib.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+        
         }
 
         public IActionResult Index()
@@ -52,10 +55,19 @@ namespace Biblioteca_UniLib.Controllers
             return View();
         }
 
+     
+
+        public IActionResult Confirmacao()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
     }
 }
